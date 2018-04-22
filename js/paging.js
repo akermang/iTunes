8b366b555dc500018a2876a0ab098f16347ff655
galakerman <akermang@gmail.com>
2018-04-22 03:34:41 +0300

    search result title fix, result min-hight, number pages display

--- js/paging.js
+++ js/paging.js
@@ -1,6 +1,6 @@
 $('#pagination-demo').twbsPagination({
     totalPages: 5,
-    visiblePages: 10,
+    visiblePages: 2,
     next: 'Next',
     prev: 'Prev',
     onPageClick: function (event, page) {
@@ -9,6 +9,7 @@ $('#pagination-demo').twbsPagination({
         let tableByPage = createTable(itemsByPage);
 
         document.getElementById("results").innerHTML = tableByPage;
+        itemsByPage.length > 0? $("tr.result-title").show(): null;
         $('#page-content').text('Page ' + page) + ' content here';
     }
 });
